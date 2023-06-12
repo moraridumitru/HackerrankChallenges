6b278694f2c7dbfd35f2c696f6cd255cb40fa2c1
moraridumitru <moraridumitru1@gmail.com>
2023-06-12 22:43:00 +0300

    FirstUpdate

--- src/JavaLoops2.java
+++ src/JavaLoops2.java
@@ -20,3 +20,12 @@ class JavaLoops2 {
         in.close();
     }
 }
+/**In this code, after reading the number of queries t, we iterate t times to process each query.
+ * For each query, we read the values of a, b, and n.
+
+ Inside the loop, we initialize a variable sum with the value of a. Then, we iterate from 0 to n-1 to calculate
+ the series. In each iteration, we add the corresponding term to the sum using the formula (2^j) * b and print it.
+ The term (2^j) * b represents the j-th term in the series. Finally, we print a newline character after each series to
+ separate them.
+ By following this approach, the program will calculate and print the series for each query based on the given values
+ of a, b, and n.**/
